f2b51ae3559615b246fdb54baa5608f20bbc3186
dontyellwillcry <papaporo@proton.me>
2024-02-06 12:48:39 -0600

    Added a 'floor' with collider. Ashaman will now act against the floor

--- Assets/AshamanScript.cs
+++ Assets/AshamanScript.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class AshamanScript : MonoBehaviour
 {
     // public Rigidbody2D myRigidbody: Creates a reference to our RigidBody2D in unity. Our script now has a place to drag and drop our component
     public Rigidbody2D myRigidbody;
+    public Transform myTransform;
 
     // I could say "speed == int;" but if i just delare a public float variable then inside of Unity there will be a new field
     // inside the script component where I can manually adjust the number.
@@ -21,9 +23,9 @@ public class AshamanScript : MonoBehaviour
     {
         // GetKeyDown: It returns true only on the frame where the key is first pressed down.  Is used for detecting the initial press of a key
         // GetKey: It returns true as long as the specified key is being held down.  Is used for detecting if a key is being held down continuously
-        if (Input.GetKey(KeyCode.W) == true)
+        if (Input.GetKey(KeyCode.Space) == true)
         {
-            myRigidbody.velocity = Vector2.up * speed;
+            myRigidbody.velocity = Vector2.up * 3;
         }
         if (Input.GetKeyDown(KeyCode.D) == true)
         {
@@ -43,6 +45,10 @@ public class AshamanScript : MonoBehaviour
             Vector2 diagonalDirection = new Vector2(1, 1).normalized;
             myRigidbody.velocity = diagonalDirection * speed;
             // myRigidbody.velocity = new Vector2(1, 1) * speed;
+        }
+        if (Input.GetKeyDown(KeyCode.G) == true)
+        {
+            myTransform.rotation = Quaternion.Euler(0, 0, 0);
         }
     }
 }
